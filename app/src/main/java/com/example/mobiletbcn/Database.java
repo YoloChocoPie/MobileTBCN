26704f4d95f4cb5f943ed2bda6539255f10d970c
YoloChocoPie <chocopieyolo@gmail.com>
2020-12-08 03:07:27 +0700

    Tạo Database

---
+++ app/src/main/java/com/example/mobiletbcn/Database.java
@@ -0,0 +1,74 @@
+package com.example.mobiletbcn;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteStatement;
+
+import com.example.mobiletbcn.model.Book;
+
+import androidx.annotation.Nullable;
+
+public class Database extends SQLiteOpenHelper {
+    public Database(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+
+    // crud data
+    public void queryData(String sql) {
+        SQLiteDatabase database = getWritableDatabase();
+        database.execSQL(sql);
+    }
+
+    // crud data
+    public void insertDataHasImage(Book book) {
+        SQLiteDatabase database = getWritableDatabase();
+        String sql = "INSERT INTO Book VALUES(NULL, ?, ?, ?, ?, ?, ?)";
+        SQLiteStatement sqLiteStatement = database.compileStatement(sql);
+        sqLiteStatement.clearBindings();
+
+        sqLiteStatement.bindString(1, book.getName());
+        sqLiteStatement.bindBlob(2, book.getImage());
+        sqLiteStatement.bindString(3, book.getPrice());
+        sqLiteStatement.bindString(4, book.getAuthor());
+        sqLiteStatement.bindString(5, book.getDescription());
+        sqLiteStatement.bindString(6, book.getQuantity());
+
+        sqLiteStatement.executeInsert();
+    }
+
+    public void UpdateDataHasImage(Book book) {
+        SQLiteDatabase database = getWritableDatabase();
+        String sql = "UPDATE Book SET name = ?, image = ?, price = ?, author = ?, description = ?, quantity = ? " +
+                "WHERE id = ?";
+        SQLiteStatement sqLiteStatement = database.compileStatement(sql);
+        sqLiteStatement.clearBindings();
+
+        sqLiteStatement.bindString(1, book.getName());
+        sqLiteStatement.bindBlob(2, book.getImage());
+        sqLiteStatement.bindString(3, book.getPrice());
+        sqLiteStatement.bindString(4, book.getAuthor());
+        sqLiteStatement.bindString(5, book.getDescription());
+        sqLiteStatement.bindString(6, book.getQuantity());
+        sqLiteStatement.bindLong(7, book.getId());
+
+        sqLiteStatement.executeInsert();
+    }
+
+    // select data
+    public Cursor getData(String sql) {
+        SQLiteDatabase database = getReadableDatabase();
+        return database.rawQuery(sql, null);
+    }
+}
--- app/src/main/java/com/example/mobiletbcn/MainActivity.java
+++ app/src/main/java/com/example/mobiletbcn/MainActivity.java
@@ -2,13 +2,58 @@ package com.example.mobiletbcn;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.mobiletbcn.Controller.BookController;
+import com.example.mobiletbcn.Controller.UserController;
 
 public class MainActivity extends AppCompatActivity {
+    Database database;
+    BookController bookController;
+    UserController userController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        database = new Database(this, "ManagementBook.sqlite", null, 1);
+
+        bookController = new BookController(database);
+        userController = new UserController(database);
+
+//        database.queryData("DROP TABLE Book");
+//        database.queryData("DROP TABLE User");
+//        database.queryData("DROP TABLE Booking");
+
+        database.queryData("CREATE TABLE IF NOT EXISTS Book (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(200), " +
+                "image BLOB, price VARCHAR(100), author VARCHAR(200), description VARCHAR(200), quantity INTEGER(100))");
+        database.queryData("CREATE TABLE IF NOT EXISTS User (id INTEGER PRIMARY KEY AUTOINCREMENT, fullName VARCHAR(200), " +
+                "role VARCHAR(20), userName VARCHAR(100), password VARCHAR(100))");
+        database.queryData("CREATE TABLE IF NOT EXISTS Booking (id INTEGER PRIMARY KEY AUTOINCREMENT, idUser_Booking INTEGER(100), " +
+                "idBook_Booking INTEGER(100))");
+
+        database.queryData("INSERT INTO User VALUES (NULL, 'Mai Tuấn Anh', 'admin', 'admin', 'admin')");
+        database.queryData("INSERT INTO User VALUES (NULL, 'Nguyễn Văn A', 'user', 'nguyenvana', '123')");
+    }
+
+    public void login(View view) {
+        EditText userName = findViewById(R.id.userName);
+        EditText password = findViewById(R.id.password);
+        String us = userName.getText().toString();
+        String pass = password.getText().toString();
+        boolean checkUser = userController.checkUserByUserNameAndPassword(us, pass);
+        if (checkUser) {
+            // thành công thì chuyển sang trang home
+            //Intent intent = new Intent(MainActivity.this, HomeMain.class);
+            //startActivity(intent);
+        } else {
+            // thất bại
+            Toast.makeText(this, "Wrong username and password", Toast.LENGTH_SHORT).show();
+        }
     }
 }
