d8b532245776d7a79968c2a5a3d588257d208e80
Nam <gaubong14092000@gmail.com>
2020-12-15 18:53:05 +0700

    Tạo Navigation cho giao diện trang chủ và tạo chức năng hiển thị List_all_book

---
+++ app/src/main/java/com/example/mobiletbcn/List_all_book.java
@@ -0,0 +1,14 @@
+package com.example.mobiletbcn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class List_all_book extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_all_book);
+    }
+}
