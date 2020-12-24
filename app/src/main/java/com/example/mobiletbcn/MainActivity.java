0c21f88225137079f4460250765c2155e9f8ad82
Long <nlong4507@gmail.com>
2020-12-24 15:23:57 +0700

    Tự động chỉnh auto role user

--- app/src/main/java/com/example/mobiletbcn/MainActivity.java
+++ app/src/main/java/com/example/mobiletbcn/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.mobiletbcn;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -17,7 +21,14 @@ public class MainActivity extends AppCompatActivity {
     Database database;
     BookController bookController;
     UserController userController;
-    Button back_icon;
+    Button back_icon,login;
+
+    CheckBox chkBoxRememberMe;
+    EditText Username, Password;
+    SharedPreferences loginPreferences;
+    SharedPreferences.Editor loginPrefsEditor;
+    boolean saveLogin;
+    private String us, pw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,21 @@ public class MainActivity extends AppCompatActivity {
         bookController = new BookController(database);
         userController = new UserController(database);
 
+        login = findViewById(R.id.button);
+        login.setOnClickListener(this::onClick);
+        Username = findViewById(R.id.userName);
+        Password = findViewById(R.id.password);
+        chkBoxRememberMe = findViewById(R.id.chkRememberMe);
+        loginPreferences = getSharedPreferences("loginPrefs", MODE_PRIVATE);
+        loginPrefsEditor = loginPreferences.edit();
+        saveLogin = loginPreferences.getBoolean("saveLogin", false);
+
+        if (saveLogin == true){
+            Username.setText(loginPreferences.getString("username", ""));
+            Password.setText(loginPreferences.getString("password", ""));
+            chkBoxRememberMe.setChecked(true);
+        }
+
         back_icon = findViewById(R.id.back_icon);
         back_icon.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,21 +66,29 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        database.queryData("DROP TABLE Book");
-//      database.queryData("DROP TABLE IF EXISTS User");
-//        database.queryData("DROP TABLE Booking");
-
-        database.queryData("CREATE TABLE IF NOT EXISTS Book (id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR(200), " +
-                "image BLOB, price VARCHAR(100), author VARCHAR(200), description VARCHAR(200), quantity INTEGER(100))");
-        database.queryData("CREATE TABLE IF NOT EXISTS User (id INTEGER PRIMARY KEY AUTOINCREMENT, fullName VARCHAR(200), " +
-                "role VARCHAR(20), userName VARCHAR(100), password VARCHAR(100), cfpass VARCHAR(100))");
-        database.queryData("CREATE TABLE IF NOT EXISTS Booking (id INTEGER PRIMARY KEY AUTOINCREMENT, idUser_Booking INTEGER(100), " +
-                "idBook_Booking INTEGER(100))");
-
-
     }
 
-    public void login(View view) {
+    public void onClick(View view){
+        if (view == login){
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(Username.getWindowToken(), 0);
+            us = Username.getText().toString();
+            pw = Password.getText().toString();
+            if (chkBoxRememberMe.isChecked()){
+                loginPrefsEditor.putBoolean("saveLogin", true);
+                loginPrefsEditor.putString("username", us);
+                loginPrefsEditor.putString("password", pw);
+                loginPrefsEditor.commit();
+            }
+            else {
+                loginPrefsEditor.clear();
+                loginPrefsEditor.commit();
+            }
+            moveToHome();
+        }
+    }
+
+    private void moveToHome() {
         EditText userName = findViewById(R.id.userName);
         EditText password = findViewById(R.id.password);
         String us = userName.getText().toString();
@@ -69,4 +103,20 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this, "Wrong username and password", Toast.LENGTH_SHORT).show();
         }
     }
+
+    /*public void login(View view) {
+        EditText userName = findViewById(R.id.userName);
+        EditText password = findViewById(R.id.password);
+        String us = userName.getText().toString();
+        String pass = password.getText().toString();
+        boolean checkUser = userController.checkUserByUserNameAndPassword(us, pass);
+        if (checkUser) {
+            // thành công thì chuyển sang trang home
+            Intent intent = new Intent(MainActivity.this, Home_screen.class);
+            startActivity(intent);
+        } else {
+            // thất bại
+            Toast.makeText(this, "Wrong username and password", Toast.LENGTH_SHORT).show();
+        }
+    }*/
 }
--- app/src/main/java/com/example/mobiletbcn/SignUpActivity.java
+++ app/src/main/java/com/example/mobiletbcn/SignUpActivity.java
@@ -3,8 +3,10 @@ package com.example.mobiletbcn;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -17,7 +19,7 @@ public class SignUpActivity extends AppCompatActivity {
     Button back_icon;
 
 
-    Database database;
+    public static Database database;
     UserController userController;
 
     @Override
@@ -25,20 +27,24 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         database = new Database(this, "ManagementBook.sqlite", null, 1);
 
+        /*back_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SignUpActivity.this, Welcom_page.class);
+                startActivity(intent);
+            }
+        });*/
 
 //        database.queryData("DROP TABLE Book");
 //        database.queryData("DROP TABLE User");
 //        database.queryData("DROP TABLE Booking");
 
-
-
-
-
         userController = new UserController(database);
         mapping();
-
     }
 
     public void mapping() {
@@ -77,9 +83,8 @@ public class SignUpActivity extends AppCompatActivity {
         return true;
     }
 
-    public void performSignUp(View view){
-        if (validateInput())
-        {
+    public void performSignUp(View view) {
+        if (validateInput()) {
             User user = new User();
             user.setFullName(etFullName.getText().toString());
             user.setRole(etRole.getText().toString());
@@ -88,49 +93,51 @@ public class SignUpActivity extends AppCompatActivity {
             user.setcfpass(etRepeatPassword.getText().toString());
 
             userController.addnewuser(user);
-            Intent intent = new Intent(this, Welcom_page.class);
-            startActivity(intent);
+            // Intent intent = new Intent(this, Welcom_page.class);
+            Toast.makeText(SignUpActivity.this, "Tạo tài khoản thành công", Toast.LENGTH_SHORT).show();
+            startActivity(new Intent(SignUpActivity.this, Welcom_page.class));
         }
+    }
+
+    public void backtowel(View view) {
+        startActivity(new Intent(SignUpActivity.this, Welcom_page.class));
+    }
+
+    /*    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        etFullName = findViewById(R.id.et_full_name);
+        etRole = findViewById(R.id.et_role);
+        etUserName = findViewById(R.id.et_user_name);
+        etPassword = findViewById(R.id.et_password);
+        etRepeatPassword = findViewById(R.id.et_repeat_password);
+        back_icon = findViewById(R.id.back_icon);
+        back_icon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUpActivity.this,Welcom_page.class);
+                startActivity(intent);
+            }
+        });
+
+    }*/
+
+    /*public void performSignUp (View v) {
+        if (validateInput()) {
+
+            String fullName = etFullName.getText().toString();
+            String role = etRole.getText().toString();
+            String userName = etUserName.getText().toString();
+            String password = etPassword.getText().toString();
+            String repeatPassword = etRepeatPassword.getText().toString();
+            userDb.createUser(fullName, role, userName, password);
+            Toast.makeText(this,"Sign Up Success",Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(SignUpActivity.this, Welcom_page.class);
+            startActivity(intent);
 
 
 
 
-
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_sign_up);
-//        etFullName = findViewById(R.id.et_full_name);
-//        etRole = findViewById(R.id.et_role);
-//        etUserName = findViewById(R.id.et_user_name);
-//        etPassword = findViewById(R.id.et_password);
-//        etRepeatPassword = findViewById(R.id.et_repeat_password);
-//        back_icon = findViewById(R.id.back_icon);
-//        back_icon.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Intent intent = new Intent(SignUpActivity.this,Welcom_page.class);
-//                startActivity(intent);
-//            }
-//        });
-//
-//    }
-
-//    public void performSignUp (View v) {
-//        if (validateInput()) {
-//
-//            String fullName = etFullName.getText().toString();
-//            String role = etRole.getText().toString();
-//            String userName = etUserName.getText().toString();
-//            String password = etPassword.getText().toString();
-//            String repeatPassword = etRepeatPassword.getText().toString();
-//            userDb.createUser(fullName, role, userName, password);
-//            Toast.makeText(this,"Sign Up Success",Toast.LENGTH_SHORT).show();
-//            Intent intent = new Intent(SignUpActivity.this, Welcom_page.class);
-//            startActivity(intent);
-//
-//
-//
-//
-//        }
-//    }
-}}
+        }
+    }*/
+}
