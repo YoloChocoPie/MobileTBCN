510fd5d5860cb7254f5534ae4b8e85dcdb70c488
Nam <gaubong14092000@gmail.com>
2020-12-25 14:18:14 +0700

    Code chức năng List_all_book

--- app/src/main/java/com/example/mobiletbcn/List_all_book.java
+++ app/src/main/java/com/example/mobiletbcn/List_all_book.java
@@ -1,14 +1,73 @@
 package com.example.mobiletbcn;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import android.os.Bundle;
+import com.example.mobiletbcn.model.Book;
 
-public class List_all_book extends AppCompatActivity {
+import java.util.List;
+
+public class List_all_book extends BaseAdapter implements View.OnClickListener {
+
+    private Context context;
+    private int layout;
+    private List<Book> bookList;
+
+    public List_all_book(Context context, int layout, List<com.example.mobiletbcn.model.Book> bookList) {
+        this.context = context;
+        this.layout = layout;
+        this.bookList = bookList;
+
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_all_book);
+    public int getCount() {
+        return bookList.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        view = layoutInflater.inflate(layout, null);
+
+        TextView textNameBook = view.findViewById(R.id.nameBook);
+        TextView textNameAuthor = view.findViewById(R.id.nameAuthor);
+        TextView textQuantity = view.findViewById(R.id.quantity);
+        ImageView imageView = view.findViewById(R.id.image);
+        TextView textType = view.findViewById(R.id.booktype);
+
+        Book book = bookList.get(i);
+        textNameBook.setText("Name: " + book.getName());
+        textNameAuthor.setText("Author: " + book.getAuthor());
+        textQuantity.setText("Số lượng: " + book.getQuantity());
+        textType.setText("Thể loại: " + book.getType());
+
+
+        // chuyển byte từ data sang bitmap và gắn vào imageView
+        byte[] image = book.getImage();
+        Bitmap bitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
+        imageView.setImageBitmap(bitmap);
+        return view;
+    }
+
+    @Override
+    public void onClick(View v) {
     }
 }
